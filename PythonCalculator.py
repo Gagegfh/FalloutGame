65532d823541cbc3f308f5ae5f1dec0a8e21fe9d
Gage Eldridge <163370684+Gagegfh@users.noreply.github.com>
2024-05-23 20:24:14 -0400

    Add files via upload

--- PythonCalculator.py
+++ PythonCalculator.py
@@ -1,18 +1,19 @@
 # My first Python project: a calculator
 
+print("Welcome to the Python Calculator!")
+print("In its current form, the Python calculator can handle whole numbers & decimals")
+print("If a user input is anything other than a number, the program will result in an error.")
+
 
 # Defining the user's global answer
 
 globalAnswer = 0
-
+            
 
 #Defining the function to do the first calculation using 2 numbers
 
 def startCalculator():
     global globalAnswer
-    print("Welcome to the Python Calculator!")
-    print("Please note, in its current form, the Python calculator can only handle whole numbers. (no decimal numbers)")
-    print("This applys to user inputs. If the answer calculated becomes a decimal, it will work fine.")
     print(" ")
     print("Please select an operation -")
     print("1 = Add")
@@ -23,29 +24,29 @@ def startCalculator():
     print(" ")
     select = int(input("Which operation would you like to use?"))
     if select == 1:
-        num_1 = int(input("Enter first number: "))
-        num_2 = int(input("Enter the number you would like to add to " + str(num_1)))
+        num_1 = float(input("Enter first number: "))
+        num_2 = float(input("Enter the number you would like to add to " + str(num_1)))
         answer = num_1 + num_2
         globalAnswer = answer
         print(" ")
         print(str(num_1) + " + " + str(num_2) + " = " + str(answer))
     elif select == 2:
-        num_1 = int(input("Enter first number: "))
-        num_2 = int(input("Enter the number you would like to subtract from " + str(num_1)))
+        num_1 = float(input("Enter first number: "))
+        num_2 = float(input("Enter the number you would like to subtract from " + str(num_1)))
         answer = num_1 - num_2
         globalAnswer = answer
         print(" ")
         print(str(num_1) + " - " + str(num_2) + " = " + str(answer))
     elif select == 3:
-        num_1 = int(input("Enter first number: "))
-        num_2 = int(input("Enter the number you would like to multiply by " + str(num_1)))
+        num_1 = float(input("Enter first number: "))
+        num_2 = float(input("Enter the number you would like to multiply by " + str(num_1)))
         answer = num_1 * num_2
         globalAnswer = answer
         print(" ")
         print(str(num_1) + " x " + str(num_2) + " = " + str(answer))
     elif select == 4:
-        num_1 = int(input("Enter first number: "))
-        num_2 = int(input("Enter the number you would like to divide by " + str(num_1)))
+        num_1 = float(input("Enter first number: "))
+        num_2 = float(input("Enter the number you would like to divide by " + str(num_1)))
         answer = num_1 / num_2
         globalAnswer = answer
         print(" ")
@@ -74,25 +75,25 @@ def additionalCalculations():
     print(" ")
     select = int(input("Which operation would you like to use?"))
     if select == 1:
-        num1 = int(input("Enter the number you would like to add to " + str(globalAnswer) + ":"))
+        num1 = float(input("Enter the number you would like to add to " + str(globalAnswer) + ":"))
         answer = globalAnswer + num1
         print(" ")
         print(str(globalAnswer) + " + " + str(num1) + " = " + str(answer))
         globalAnswer = answer
     elif select == 2:
-        num1 = int(input("Enter the number you would like to subtract from " + str(globalAnswer) + ":"))
+        num1 = float(input("Enter the number you would like to subtract from " + str(globalAnswer) + ":"))
         answer = globalAnswer - num1
         print(" ")
         print(str(globalAnswer) + " - " + str(num1) + " = " + str(answer))
         globalAnswer = answer
     elif select == 3:
-        num1 = int(input("Enter the number you would like to multiply by " + str(globalAnswer) + ":"))
+        num1 = float(input("Enter the number you would like to multiply by " + str(globalAnswer) + ":"))
         answer = globalAnswer * num1
         print(" ")
         print(str(globalAnswer) + " x " + str(num1) + " = " + str(answer))
         globalAnswer = answer
     elif select == 4:
-        num1 = int(input("Enter the number you would like to divide by " + str(globalAnswer) + ":"))
+        num1 = float(input("Enter the number you would like to divide by " + str(globalAnswer) + ":"))
         answer = globalAnswer / num1
         print(" ")
         print(str(globalAnswer) + " / " + str(num1) + " = " + str(answer))
